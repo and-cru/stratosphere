41fd513521e537e81b4033b2c8eb3ea8966c51c0
pedreviljoen <pedre87@gmail.com>
2021-03-10 08:58:52 +0200

    Wrapped up ECR testing -> Creation, happy path

--- test/ecr.test.js
+++ test/ecr.test.js
@@ -5,4 +5,34 @@ test('Jest mock', () => {
     expect(x).toBe(2)
 })
 
-// ECR testing
+// ECR testing - Creation - Happy path
+
+test('Create PublicRepository', () => {
+    const item = new ecr.PublicRepository('Sample')
+
+    expect(item.name).toBe('Sample')
+    expect(item.type).toBe('AWS::ECR::PublicRepository')
+})
+
+test('Create RegistryPolicy', () => {
+    const item = new ecr.RegistryPolicy('Test')
+
+    expect(item.name).toBe('Test')
+    expect(item.type).toBe('AWS::ECR::RegistryPolicy')
+})
+
+test('Create ReplicationConfiguration', () => {
+    const item = new ecr.ReplicationConfiguration('Sample')
+
+    expect(item.name).toBe('Sample')
+    expect(item.type).toBe('AWS::ECR::ReplicationConfiguration')
+})
+
+test('Create Repository', () => {
+    const item = new ecr.Repository('Test')
+
+    expect(item.name).toBe('Test')
+    expect(item.type).toBe('AWS::ECR::Repository')
+})
+
+// ECR Testing - Properties - Sad Path
