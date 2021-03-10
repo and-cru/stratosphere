4b9295b2b08dde2fdb51f7010f8dfdd215fe2f52
pedreviljoen <pedre87@gmail.com>
2021-03-10 12:43:55 +0200

    Added S3 testing

--- test/s3.test.js
+++ test/s3.test.js
@@ -6,3 +6,31 @@ test('Jest mock', () => {
 })
 
 // S3 testing
+
+test('Create AccessPoint', () => {
+    const item = new s3.AccessPoint('Test')
+
+    expect(item.name).toBe('Test')
+    expect(item.type).toBe('AWS::S3::AccessPoint')
+})
+
+test('Create Bucket', () => {
+    const item = new s3.Bucket('Test')
+
+    expect(item.name).toBe('Test')
+    expect(item.type).toBe('AWS::S3::Bucket')
+})
+
+test('Create BucketPolicy', () => {
+    const item = new s3.BucketPolicy('Test')
+
+    expect(item.name).toBe('Test')
+    expect(item.type).toBe('AWS::S3::BucketPolicy')
+})
+
+test('Create StorageLens', () => {
+    const item = new s3.StorageLens('Test')
+
+    expect(item.name).toBe('Test')
+    expect(item.type).toBe('AWS::S3::StorageLens')
+})
